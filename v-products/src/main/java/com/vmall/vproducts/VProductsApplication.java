f5f4378ee2990f1ebb1ad08ff3b60fb73e08e778
CC0115 <cc925383199@163.com>
2019-04-27 07:49:55 +0800

    chenchang

--- v-products/src/main/java/com/vmall/vproducts/VProductsApplication.java
+++ v-products/src/main/java/com/vmall/vproducts/VProductsApplication.java
@@ -1,14 +1,14 @@
 package com.vmall.vproducts;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.vmall.mapper.*")
 public class VProductsApplication {
 
     public static void main(String[] args) {
-
-        System.out.println();
         SpringApplication.run(VProductsApplication.class, args);
     }
 
---
+++ v-products/src/main/java/com/vmall/vproducts/controller/vcategory/VCategoryController.java
@@ -0,0 +1,8 @@
+package com.vmall.vproducts.controller.vcategory;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VCategoryController {
+
+}
