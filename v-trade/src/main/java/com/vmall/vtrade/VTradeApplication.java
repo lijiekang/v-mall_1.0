d37198ca0e6fed16ce8eb4681fe53e96e3fa68cf
luyaoba <licanhui0311@163.com>
2019-04-26 18:52:14 +0800

    canhui

--- v-trade/src/main/java/com/vmall/vtrade/VTradeApplication.java
+++ v-trade/src/main/java/com/vmall/vtrade/VTradeApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VTradeApplication {
 
     public static void main(String[] args) {
-
-        System.out.println();
         SpringApplication.run(VTradeApplication.class, args);
     }
 }
